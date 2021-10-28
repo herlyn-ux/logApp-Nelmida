74251b844e5cd96330ad7ff543faa5d6b4cb06aa
herlyn-ux <herlynpassionnelmida@gmail.com>
2021-10-28 09:57:10 +0800

    Cloned logApp-scaling-octo

---
+++ logApp-Nelmida/config/config.php
@@ -0,0 +1,9 @@
+<?php
+	define('ROOT_URL', 'http://localhost/legendary-garbanzo/logApp-scaling-octo-rubyjets/index.php');
+	define('DB_HOST', '127.0.0.1:3325');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'user');
+
+
+?>
---
+++ logApp-Nelmida/inc/footer.php
@@ -0,0 +1,9 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/DKexVann68">Dyla Bondesto</a>, 
+      <a href="https://github.com/MarkCPT21">Mark Christian Tabang</a>, 
+      and <a href="https://github.com/herlyn-ux">Herlyn Nelmida</a> for BSCS 3B1</p>
+    </div>
+</footer>
+  </body>
+</html>
