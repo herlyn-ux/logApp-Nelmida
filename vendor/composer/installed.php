d90fa45df6a111d730fbf11d5efc06bf91dfaa5f
herlyn-ux <herlynpassionnelmida@gmail.com>
2021-10-29 06:37:54 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '78403ba276efb1746ab4212adfccc2c41f500748',
+        'name' => 'herlyn-ux/logapp-nelmida',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'herlyn-ux/logapp-nelmida' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '78403ba276efb1746ab4212adfccc2c41f500748',
+            'dev_requirement' => false,
+        ),
+    ),
+);
