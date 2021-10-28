78403ba276efb1746ab4212adfccc2c41f500748
herlyn-ux <herlynpassionnelmida@gmail.com>
2021-10-29 06:12:44 +0800

    Done Practice set-06

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/logApp-Nelmida/logApp-Nelmida/index.php');
+	define('ROOT_URL', 'http://localhost/logApp-Nelmida/index.php');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447299');
 	define('DB_PASS', 'xQF8f3CemC');
